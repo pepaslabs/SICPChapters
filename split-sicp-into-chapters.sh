c0c6dd7cc1c7d033e1013ef13b759c887a85a523
Jason Pepas <jasonpepas@gmail.com>
2015-12-03 20:48:53 -0600

    fixing page numbers

---
+++ split-sicp-into-chapters.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+pdftk A=sicp.pdf cat A29-134 output sicp_chapter_1.pdf
+pdftk A=sicp.pdf cat A135-321 output sicp_chapter_2.pdf
+pdftk A=sicp.pdf cat A322-514 output sicp_chapter_3.pdf
+pdftk A=sicp.pdf cat A515-693 output sicp_chapter_4.pdf
+pdftk A=sicp.pdf cat A694-861 output sicp_chapter_5.pdf
