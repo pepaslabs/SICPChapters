3687c4c52c679ddd8b48ffd9c7252814017557cf
Jason Pepas <jasonpepas@gmail.com>
2015-12-03 21:08:58 -0600

    adding comment for debian users

--- split-sicp-into-chapters.sh
+++ split-sicp-into-chapters.sh
@@ -2,6 +2,8 @@
 
 set -eu -o pipefail
 
+# sudo apt-get install pdftk
+
 pdftk A=sicp.pdf cat A29-134 output sicp_chapter_1.pdf
 pdftk A=sicp.pdf cat A135-321 output sicp_chapter_2.pdf
 pdftk A=sicp.pdf cat A322-514 output sicp_chapter_3.pdf
